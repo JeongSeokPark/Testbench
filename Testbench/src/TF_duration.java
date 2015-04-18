7f8158fbc84398afcf5f42a43c3457eb146c861f
JeongSeokPark <black3184@nate.com>
2015-04-19 02:12:55 +0900

    TF_duration completed.

--- Testbench/src/TF_duration.java
+++ Testbench/src/TF_duration.java
@@ -9,9 +9,12 @@ public class TF_duration extends JTextField implements KeyListener{
 	/**
 	 * 
 	 */
+	private boolean dot_flag = false;
+	private String str;
+	
 	private static final long serialVersionUID = 1L;
 	public TF_duration(){
-		addKeyListener(this);
+		
 	}
 
 	@Override
@@ -23,15 +26,31 @@ public class TF_duration extends JTextField implements KeyListener{
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if(!Character.isDigit(e.getKeyChar()) && e.getKeyChar() != 8 && e.getKeyChar() != 45 && e.getKeyChar() != 46){
-			e.consume();
+		str = getText();
+		
+		if(!str.contains(".")){ // If dot is not exist current text field
+			dot_flag = false;
 		}
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		if(!dot_flag){ // Dot not typed yet.
+			if(!Character.isDigit(e.getKeyChar()) && e.getKeyChar() != 8 && e.getKeyChar() != 45 && e.getKeyChar() != 46){
+				// 8 = back space, 45 = minus, 46 = dot
+				e.consume();
+			}
+			if(e.getKeyChar() == 46){
+				dot_flag = true;
+			}
+		}
+		else{ // Dot typed.
+			if(!Character.isDigit(e.getKeyChar()) && e.getKeyChar() != 8 && e.getKeyChar() != 45){
+				// 8 = back space, 45 = minus, 46 = dot
+				e.consume();
+			}
+		}
 	}
 
 }
